da950c5af0301023224a1b925662e9a5a940ea2e
Amarante <felipeamarante@gmail.com>
2017-03-02 06:41:26 +0200

    alexa api

--- app.py
+++ app.py
@@ -61,10 +61,11 @@ def resetCounter():
     return redirect("")
 
 
+@app.route("/alexa")
+def alexa():
+    return totalSurto
 
 
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=False)
-
-
